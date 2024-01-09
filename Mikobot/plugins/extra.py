7f665b6d547b09cd6c221ff1f0e0b1fb02b45f20
⚡️MOON⚡️ <149287870+Moonshining6@users.noreply.github.com>
2024-01-09 02:21:21 -0800

    Update extra.py

--- Mikobot/plugins/extra.py
+++ Mikobot/plugins/extra.py
@@ -100,7 +100,7 @@ async def ping(_, m: Message):
     delta_ping = time() - start
 
     up = strftime("%Hh %Mm %Ss", gmtime(time() - UPTIME))
-    image_url = "https://telegra.ph/file/f215a1c4adf25a5bad81b.jpg"
+    image_url = "https://telegra.ph/file/2a758d72b3d16bb349287.jpg"
 
     # Send the image as a reply
     await replymsg.reply_photo(
