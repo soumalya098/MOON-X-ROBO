f01341c73e97e06d51fe7da5db4200944d251c67
Soumalya <129831114+soumalya098@users.noreply.github.com>
2024-01-23 14:00:00 +0530

    Add files via upload

---
+++ Infamous/karma.py
@@ -0,0 +1,105 @@
+# https://github.com/Infamous-Hydra/YaeMiko
+# https://github.com/Team-ProjectCodeX
+# https://t.me/O_okarma
+
+# <============================================== IMPORTS =========================================================>
+from pyrogram.types import InlineKeyboardButton as ib
+from telegram import InlineKeyboardButton
+
+from Mikobot import BOT_USERNAME, OWNER_ID, SUPPORT_CHAT
+
+# <============================================== CONSTANTS =========================================================>
+START_IMG = [
+    "https://telegra.ph/file/be1b39e8961764340ca47.jpg",
+    "https://telegra.ph/file/7451cf62114666ef941a5.jpg",
+    "https://telegra.ph/file/be1b39e8961764340ca47.jpg",
+    "https://telegra.ph/file/7451cf62114666ef941a5.jpg",
+    "https://telegra.ph/file/be1b39e8961764340ca47.jpg",
+    "https://telegra.ph/file/7451cf62114666ef941a5.jpg",
+    "https://telegra.ph/file/be1b39e8961764340ca47.jpg",
+]
+
+HEY_IMG = "https://telegra.ph/file/7451cf62114666ef941a5.jpg"
+
+ALIVE_ANIMATION = [
+    "https://telegra.ph//file/f9e2b9cdd9324fc39970a.mp4",
+    "https://telegra.ph//file/8d4d7d06efebe2f8becd0.mp4",
+    "https://telegra.ph//file/c4c2759c5fc04cefd207a.mp4",
+    "https://telegra.ph//file/b1fa6609b1c4807255927.mp4",
+    "https://telegra.ph//file/f3c7147da6511fbe27c25.mp4",
+    "https://telegra.ph//file/39071b73c02e3ff5945ca.mp4",
+    "https://telegra.ph//file/8d4d7d06efebe2f8becd0.mp4",
+    "https://telegra.ph//file/6efdd8e28756bc2f6e53e.mp4",
+]
+
+BAN_GIFS = [
+    "https://telegra.ph/file/00f29ec4f2a00862ad2f6.jpg",
+]
+
+
+KICK_GIFS = [
+    "https://telegra.ph/file/d2cddcc3fc0c4af67eb6e.jpg",
+]
+
+
+MUTE_GIFS = [
+    "https://telegra.ph/file/b783717edf84f2e3f8424.jpg",
+]
+
+FIRST_PART_TEXT = "✨ *ʜᴇʟʟᴏ* `{}` . . ."
+
+PM_START_TEXT = "✨ *ɪ ᴀᴍ ˹˹˹Muskan ✘𝙍𝙤𝙗𝙤 ˼ ~🥀 ˼, ᴀ ɢᴇɴꜱʜɪɴ ɪᴍᴘᴀᴄᴛ ᴛʜᴇᴍᴇᴅ ʀᴏʙᴏᴛ ᴡʜɪᴄʜ ᴄᴀɴ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ᴍᴀɴᴀɢᴇ ᴀɴᴅ ꜱᴇᴄᴜʀᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴡɪᴛʜ ʜᴜɢᴇ ɢʀᴏᴜᴘ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ*"
+
+START_BTN = [
+    [
+        InlineKeyboardButton(
+            text="⇦ ADD ME ⇨",
+            url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+        ),
+    ],
+    [
+        InlineKeyboardButton(text="HELP", callback_data="help_back"),
+    ],
+    [
+        InlineKeyboardButton(text="DETAILS", callback_data="Miko_"),
+        InlineKeyboardButton(text="AI", callback_data="ai_handler"),
+        InlineKeyboardButton(text="SOURCE", callback_data="git_source"),
+    ],
+    [
+        InlineKeyboardButton(text="CREATOR", url=f"tg://user?id={OWNER_ID}"),
+    ],
+]
+
+GROUP_START_BTN = [
+    [
+        InlineKeyboardButton(
+            text="⇦ ADD ME ⇨",
+            url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+        ),
+    ],
+    [
+        InlineKeyboardButton(text="SUPPORT", url=f"https://t.me/{SUPPORT_CHAT}"),
+        InlineKeyboardButton(text="CREATOR", url=f"tg://user?id={OWNER_ID}"),
+    ],
+]
+
+ALIVE_BTN = [
+    [
+        ib(text="UPDATES", url="https://t.me/foreveer_ms"),
+        ib(text="SUPPORT", url="https://t.me/galaxy_chatting_group"),
+    ],
+    [
+        ib(
+            text="⇦ ADD ME ⇨",
+            url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+        ),
+    ],
+]
+
+HELP_STRINGS = """
+🫧 *˹ Muskan ✘𝙍𝙤𝙗𝙤 ˼ ~🥀 ˼* 🫧
+
+☉ *Here, you will find a list of all the available commands.*
+
+ᴀʟʟ ᴄᴏᴍᴍᴀɴᴅs ᴄᴀɴ ʙᴇ ᴜsᴇᴅ ᴡɪᴛʜ : /
+"""
